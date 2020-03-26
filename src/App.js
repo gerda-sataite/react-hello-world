0598fee0bc567159175d289c7fea94e0167e0f85
gerda-sataite <gerda.sataite@gmail.com>
2020-03-26 09:57:43 +0000

    Added columns to the page

--- src/App.js
+++ src/App.js
@@ -13,22 +13,34 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <hr />
+          
           NOODLELAND!!!
         </p>
         
         
-        <p><img src={icon} className="App-icon" alt="icon" /></p>
-        
-        <p>
-        <Noodle> </Noodle> 
-        </p>
+        <div class="row">
+          <div class="column">
+            <p>
+              <Noodle> </Noodle> 
+            </p>
+          </div>
 
-        <p> <Jumbo fluid> </Jumbo> </p>
-        <p> Likes: </p>
-        <p><Horizontal item1="Bite toes" item2="Eat" item3="Sleep" item4="Being a little shit"> </Horizontal></p>
-        <p> Dislikes: </p>
-        <p><Horizontal item1="Being good" item2="Using the litterbox" item3="Salmon oil" item4="Eggs"> </Horizontal></p>
+          <div class="column">
+              <p> <Jumbo fluid> </Jumbo> </p>
+               <p> Likes: </p>
+               <p><Horizontal item1="Bite toes" item2="Eat" item3="Sleep" item4="Being a little shit"> </Horizontal></p>
+              <p> Dislikes: </p>
+               <p><Horizontal item1="Being good" item2="Using the litterbox" item3="Salmon oil" item4="Eggs"> </Horizontal></p>
+          </div>
+
+          <div class="column">
+            
+            
+            
+          </div>      
+
+        </div>
+
         
         
 
