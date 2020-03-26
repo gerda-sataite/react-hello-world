ba9233fc49143e717fb29c0590c1fcbe24387252
gerda-sataite <gerda.sataite@gmail.com>
2020-03-26 10:51:39 +0000

    Added spacing between columns and added 3rd column with test comment

--- src/App.js
+++ src/App.js
@@ -9,32 +9,32 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-          NOODLELAND!!!
-        </p>
-        
-        
-        <div class="row">
+        <p />
+        <div class="grid">
           <div class="column">
+            <p>
+          
+               NOODLELAND!!!
+            </p>
             <p>
               <Noodle> </Noodle> 
             </p>
           </div>
 
           <div class="column">
-              <p> <Jumbo fluid> </Jumbo> </p>
+               <p><Jumbo fluid> </Jumbo> </p>
                <p> Likes: </p>
                <p><Horizontal item1="Bite toes" item2="Eat" item3="Sleep" item4="Being a little shit"> </Horizontal></p>
-              <p> Dislikes: </p>
+               <p> Dislikes: </p>
                <p><Horizontal item1="Being good" item2="Using the litterbox" item3="Salmon oil" item4="Eggs"> </Horizontal></p>
           </div>
 
           <div class="column">
-            
+            <p>I am Noodle. Conqueror of Worlds.</p>
             
             
           </div>      
