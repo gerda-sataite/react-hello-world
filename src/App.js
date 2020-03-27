1c51589975538f402c61214ffa226cfddf821f64
gerda-sataite <gerda.sataite@gmail.com>
2020-03-27 10:06:31 +0000

    Beautified all code components and main files

--- src/App.js
+++ src/App.js
@@ -12,59 +12,59 @@ import './App.css';
 
 function App() {
   return (
-    
+
     <div className="App">
       <header className="App-header">
-        <p /> 
-        <img src={logo} className="App-logo" alt="logo" /> 
         <p />
-        
+        <img src={logo} className="App-logo" alt="logo" />
+        <p />
+
         <div class="grid">
           <div class="column">
             <h1>
-               NOODLELAND!!!
-            </h1> 
-            <p> <Noodle> </Noodle> </p> 
+              NOODLELAND!!!
+            </h1>
+            <p> <Noodle> </Noodle> </p>
             <p><Music /></p>
           </div>
 
           <div class="column">
-               <p><Jumbo> </Jumbo> </p>
-               <p><b>Likes: </b> </p>
-               <p><Horizontal
-                    item1="Bite toes" 
-                    item2="Eat" 
-                    item3="Sleep" 
-                    item4="Being a little rascal"> 
-                    </Horizontal>
-                    </p>
+            <p><Jumbo> </Jumbo> </p>
+            <p><b>Likes: </b> </p>
+            <p><Horizontal
+              item1="Bite toes"
+              item2="Eat"
+              item3="Sleep"
+              item4="Being a little rascal">
+            </Horizontal>
+            </p>
 
-               <p><b>Dislikes: </b></p>
-               <p><ListGroup 
-               item1="Being good" 
-               item2="Using the litterbox" 
-               item3="Salmon oil" 
-               item4="Eggs"> 
-               </ListGroup>
-               </p>
+            <p><b>Dislikes: </b></p>
+            <p><ListGroup
+              item1="Being good"
+              item2="Using the litterbox"
+              item3="Salmon oil"
+              item4="Eggs">
+            </ListGroup>
+            </p>
           </div>
 
           <div class="column">
             <p><img src={icon} className="App-icon" alt="icon" /></p>
-            <p><Information 
-              age='8 months' 
-              nationality="Noodlish" 
-              personality='Danger' 
-              powers='Invulnerability' 
-              />
-              </p>
+            <p><Information
+              age='8 months'
+              nationality="Noodlish"
+              personality='Danger'
+              powers='Invulnerability'
+            />
+            </p>
 
-          </div>      
+          </div>
 
         </div>
 
-        
-        
+
+
 
       </header>
     </div>
